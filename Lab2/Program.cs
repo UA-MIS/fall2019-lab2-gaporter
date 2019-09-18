8352eed92054874c72eda74723f0f55357bcf5f9
GeorgePorter <gaporter@crimson.ua.edu>
2019-09-18 11:27:25 -0500

    I think i figured it out?

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,8 +11,19 @@ namespace Lab2
         static void Main(string[] args)
         {
             //Starter code for lab 2
-            
+           double height = 0;
+            double steps = 0;
+            double miles = 0;
 
+            Console.WriteLine("enter your height in inches");
+           height = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter your steps");
+            steps = double.Parse(Console.ReadLine());
+
+
+            miles = ((((height * .413)/12) * steps)/5280);
+            Console.WriteLine("you walked " + miles + " miles");
+            Console.ReadKey();
         }
     }
 }
